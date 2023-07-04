e5219d3b70c66d89b5f7e27764f5554044d55f9e
Moro <amaru89@yandex.ru>
2023-07-04 16:25:06 +0300

    add capabilities methods(Listener,addAttachments)

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,7 +2,15 @@ package tests;
 
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
 public class TestBase {
 
 
@@ -13,9 +21,32 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
         //Configuration.browserSize = "1366x768";
-        Configuration.browser = "Firefox";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
         Configuration.holdBrowserOpen = true;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
     }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+
 }
 
 
